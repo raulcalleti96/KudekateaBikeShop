a554f046e2c84e751fc3572d7607e8f15a44b4a4
RAÚL SANTIAGO PRIETO <raulcalleti96@icloud.com>
2021-01-21 18:51:32 +0100

    Diseño Login

--- KudekateaBikeShop/Login.Designer.cs
+++ KudekateaBikeShop/Login.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace KudekateaBikeShop
+namespace ProyectoFinal
 {
     partial class Login
     {
@@ -30,22 +30,106 @@ namespace KudekateaBikeShop
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KUDAKATEA BIKE SHOP";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(296, 148);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 44);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(296, 286);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(277, 44);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::KudekateaBikeShop.Properties.Resources.icons8_login_48;
+            this.pictureBox3.Location = new System.Drawing.Point(380, 415);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(83, 83);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::KudekateaBikeShop.Properties.Resources.icons8_forgot_password_48;
+            this.pictureBox2.Location = new System.Drawing.Point(158, 269);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(83, 83);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KudekateaBikeShop.Properties.Resources.icons8_member_skin_type_7_48;
+            this.pictureBox1.Location = new System.Drawing.Point(158, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1107, 555);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(788, 555);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Login";
             this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- KudekateaBikeShop/Login.cs
+++ KudekateaBikeShop/Login.cs
@@ -8,13 +8,106 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace KudekateaBikeShop
+namespace ProyectoFinal
 {
     public partial class Login : Form
     {
+
+        public static Login f;
+
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void empleadosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.empleadosBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bBDDVideojuegosDataSet);
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'bBDDVideojuegosDataSet.Empleados' Puede moverla o quitarla según sea necesario.
+            this.empleadosTableAdapter.Fill(this.bBDDVideojuegosDataSet.Empleados);
+
+            CenterToScreen();
+
+            usuarioTextBox.Text = "";
+            passwordTextBox.Text = "";
+
+            f = this;
+
+        }
+
+        private void btnAcceder_Click(object sender, EventArgs e)
+        {
+            BBDDVideojuegosDataSet.EmpleadosRow fila;//creamos una variable de tipo fila
+
+            if (empleadosBindingSource.Find("Usuario", usuarioTextBox.Text) != -1)//comprobamos que es distinto de no es correcto
+            {
+                //hacemos que busque al usuario
+                fila = (BBDDVideojuegosDataSet.EmpleadosRow)bBDDVideojuegosDataSet.Empleados.Rows[empleadosBindingSource.Find("Usuario", usuarioTextBox.Text)];
+
+                if (fila.Password == passwordTextBox.Text)//comprbamos que la clave es correcta
+                {
+                    Properties.Settings.Default.Rol = fila.Rol;//guardamos el rol en la variable global
+                    Properties.Settings.Default.Usuario = fila.Usuario;
+
+                    //MessageBox.Show(fila.Password, "Error");
+                    //MessageBox.Show(passwordTextBox.Text, "Error");
+
+                    //escondemos esta ventana y vaciamos los txt
+                    f.Hide();
+                    usuarioTextBox.Text = "";
+                    passwordTextBox.Text = "";
+
+                    //abrimos la ventana
+                    Menu p = new Menu();
+                    p.ShowDialog();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("La contraseña es incorrecta", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (usuarioTextBox.Text.Equals("") | passwordTextBox.Text.Equals(""))
+            {
+                MessageBox.Show("No puede haber campos vacios", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("El usuario es incorrecto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnInformacion_Click(object sender, EventArgs e)
+        {
+            Boolean abierta = false;
+
+            foreach (Form fm in Application.OpenForms)
+            {
+                if (fm.GetType() == typeof(Informacion))
+                {
+                    if (fm.WindowState == FormWindowState.Minimized)
+                    {
+                        fm.WindowState = FormWindowState.Normal;
+                    }
+                    fm.BringToFront();
+                    abierta = true;
+                    break;
+                }
+            }
+
+            if (!abierta)
+            {
+                Informacion v2 = new Informacion();
+                v2.Show();
+            }
         }
     }
 }
